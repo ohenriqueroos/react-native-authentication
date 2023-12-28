7c64e6c72159b3ae2fa0aba2fe5b5762c38914a6
henrique.roos <henrique.roos@egadnet.com.br>
2023-12-28 13:13:56 -0300

    first commit

---
+++ screens/LoginScreen.js
@@ -0,0 +1,33 @@
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
+
+function LoginScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
+
+  const signinHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not log you in. Please check your credentials or try again later!"
+      );
+      setIsAuthenticating(false);
+    }
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you ind..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={signinHandler} />;
+}
+
+export default LoginScreen;
---
+++ util/auth.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyDkJrSGwdoMSUvVlznbVzO4luWqO9k45AM";
+
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+
+  return response.data.idToken;
+};
+
+export const createUser = (email, password) => {
+  return authenticate("signUp", email, password);
+};
+
+export const login = (email, password) => {
+  return authenticate("signInWithPassword", email, password);
+};
